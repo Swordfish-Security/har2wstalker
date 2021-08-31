3913ff61b6b5c20a696f5259fb818dee87b1618c
shad0wrunner <shadowrunner3k@gmail.com>
2021-08-31 20:19:12 +0300

    Auto-add Host header for HTTP/2 and HTTP/1 requests Auto append a correct HTTP version Include PostBody in every request that has it

--- har_to_wstalker.py
+++ har_to_wstalker.py
@@ -3,8 +3,10 @@ import json
 from haralyzer_3 import *
 from urllib.parse import urlparse
 
-HAR_FILENAME = 'stingray_more.har'  # file in HAR format
-CSV_FILENAME = 'stingray_more.csv'  # file to import into the plugin
+HAR_FILENAME = 'data.har'  # file in HAR format
+CSV_FILENAME = 'data.csv'  # file to import into the plugin
+
+
 nl = '\r\n'
 wstalker_out = ''
 
@@ -12,14 +14,23 @@ with open(HAR_FILENAME, 'r') as har_file:
     har = HarParser(json.loads(har_file.read()))
     har_page_entries = HarPage("unknown", har_parser=har).entries
 
+
 for har_page_entry in har_page_entries:
     # Getting raw headers dictionary from the HAR contents
     req_raw_headers = har_page_entry.request.raw_entry['headers']
     resp_raw_headers = har_page_entry.response.raw_entry['headers']
 
+    # Some HTTP requests (esp. in HTTP/2) doesn't have a specific Host: header so we'll build it ourselves
+    if not har_page_entry.request.host:
+        req_raw_headers.append({'name': 'Host', 'value': urlparse(har_page_entry.request.url).netloc})
+
     # Building a query string for request (ex, GET / HTTP/1.1) and server response code
-    req_query = '%s %s HTTP/1.1' % (har_page_entry.request.method, urlparse(har_page_entry.request.url).path)
-    resp_code = 'HTTP/1.1 %s %s' % (har_page_entry.response.status, har_page_entry.response.statusText)
+    req_query = '%s %s %s' % (har_page_entry.request.method,
+                              urlparse(har_page_entry.request.url).path,
+                              har_page_entry.request.httpVersion)
+    resp_code = '%s %s %s' % (har_page_entry.request.httpVersion,
+                              har_page_entry.response.status,
+                              har_page_entry.response.statusText)
 
     # Building HTTP headers for requests and response
     req_headers = ''
@@ -31,20 +42,17 @@ for har_page_entry in har_page_entries:
 
     # POST contents
     post_body = ''
-    if har_page_entry.request.method in ['POST', 'PUT']:
+    if 'postData' in har_page_entry.request:
         post_body = har_page_entry.request['postData']['text'] + nl
     # TODO: if needed it is possible to return response data as well
     req = req_query + nl + req_headers + nl + post_body
     resp = resp_code + nl + resp_headers
-    print(req+nl+resp)
 
     # encoding to base64 according to wstalker format: b64(req),b64(resp),httpMethod,URL
-    wstalker_out += base64.b64encode(req.encode('ascii')).decode() + ','
-    wstalker_out += base64.b64encode(resp.encode('ascii')).decode() + ','
+    wstalker_out += base64.b64encode(req.encode()).decode() + ','
+    wstalker_out += base64.b64encode(resp.encode()).decode() + ','
     wstalker_out += har_page_entry.request.method + ',' + har_page_entry.request.url
     wstalker_out += nl
-
-print(wstalker_out)
 
 with open(CSV_FILENAME, 'wb') as csv_file:
     csv_file.write(wstalker_out.encode('ascii'))
